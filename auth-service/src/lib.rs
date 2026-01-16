a1cae9360c4c9adde4427ccdfd4523ab75930449
Marcio Garcia <marcio.ca.garcia@gmail.com>
2026-01-16 18:07:51 -0300

    [Sprint 1][Task 4] Implement routes using TDD

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,8 +1,13 @@
 use std::error::Error;
-use axum::{response::Html, routing::get, serve, Router};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse},
+    routing::{get_service, post},
+    Router
+};
 use axum::serve::Serve;
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -14,9 +19,19 @@ pub struct Application {
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        let assets_dir = ServeDir::new("assets");
+        // let assets_dir = ServeDir::new("assets")
+        //     .not_found_service(ServeFile::new("assets/index.html"));
+        let assets = get_service(
+            ServeDir::new("assets")
+                .not_found_service(ServeFile::new("assets/index.html"))
+        );
         let router = Router::new()
-            .fallback_service(assets_dir);
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token))
+            .fallback_service(assets);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -29,4 +44,26 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+// Example route handler.
+// For now we will simply return a 200 (OK) status code.
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
